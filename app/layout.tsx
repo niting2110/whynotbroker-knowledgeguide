bc79b92eb21486da88525a5c8db114308aabb784
niting2110 <niting2110@gmail.com>
2025-12-30 18:01:20 +0000

    Push these changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,11 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.className} bg-white text-black antialiased dark:bg-gray-950 dark:text-white`}>
+      {/* suppressHydrationWarning added to body to prevent Grammarly/Extension crashes */}
+      <body 
+        className={`${inter.className} bg-white text-black antialiased dark:bg-gray-950 dark:text-white`}
+        suppressHydrationWarning
+      >
         <ThemeProviders>
           <div className="flex h-screen flex-col justify-between font-sans">
             <Header />
